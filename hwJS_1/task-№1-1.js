01cfe3167c81ffbf7dfd135e37f76007d978a645
Holovchyk <golovchuk.misha@gmail.com>
2020-04-06 19:03:18 +0300

    initial commit

--- hwJS_1/task-№1-1.js
+++
@@ -1,5 +0,0 @@
-const name = "Генератор защитного поля";
-const price = "1000";
-const priceNew = "2000";
-
-console.log(`Выбран ${name} , цена за штуку ${priceNew} кредитов`);
---
+++ hwJS_1/task-№1_1-6.js
@@ -0,0 +1,110 @@
+// const name = "Генератор защитного поля";
+// const price = "1000";
+// const priceNew = "2000";
+
+// console.log(`Выбран ${name} , цена за штуку ${priceNew} кредитов`);
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+
+// const total = 100;
+// const ordered = 11;
+
+// if (total >= ordered) {
+//   alert("Заказ оформлен, с вами свяжется менеджер");
+// } else alert("На складе недостаточно твоаров!");
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+
+
+// const ADMIN_PASSWORD = 'jqueryismyjam';
+
+// let message;
+
+// const password = prompt('Введите ваш пароль')
+
+// if (password === ADMIN_PASSWORD) {
+//     message = 'Добро пожаловать!'
+// } else if (password === null) {
+//     message = 'Отменено пользователем!'
+// } else if (password !== ADMIN_PASSWORD) {
+//     message = 'Доступ запрещен, неверный пароль!'
+// }
+
+// alert(message)
+
+
+//=+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++=
+
+
+// const credit = 23580;
+// const pricePerDroid = 3000;
+// let totalPrice
+// let price
+// let input;
+
+// input = prompt("Сколько дроидов вы хотите купить?");
+
+// totalPrice = input * pricePerDroid
+// price = credit - totalPrice
+// if(input === null) {
+// console.log('Отменено пользователем!')
+// } else if (totalPrice >= credit) {
+//     console.log('Недостаточно средств на счету!')
+// } else {
+//     console.log(`Вы купили ${input} дроидов, на счету осталось ${price}`)
+// }
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const answer = prompt ("your country");
+// let price;
+
+// switch (answer.toLowerCase()) {
+//   case "китай":
+//     price = "100 ";
+//     alert(`Доставка в Китай будет стоить ${price} кредитов`)
+//     break;
+//   case "чили":
+//     price = "250 ";
+//     alert(`Доставка в Чили будет стоить ${price} кредитов`)
+//     break;
+//   case "австралия":
+//     price = "170 ";
+//     alert(`Доставка в Австралия будет стоить ${price} кредитов`)
+//     break;
+//   case "индия":
+//     price = "80 ";
+//     alert(`Доставка в Индия будет стоить ${price} кредитов`)
+//     break;
+//   case "ямайка":
+//     price = "120 ";
+//     alert(`Доставка в Ямайка будет стоить ${price} кредитов`)
+//     break;
+//   default:
+//     alert("В вашей стране доставка не доступна");
+// }
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// let total = 0;
+// let input;
+
+// while (true) {
+//   input = prompt("Ведите число!!!");
+//   if (input === null) {
+//     break;
+//   }
+//   input = Number(input);
+//   total += input;
+// }
+
+// alert(`Общая сумма чисел равна ${(total)}`);
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
