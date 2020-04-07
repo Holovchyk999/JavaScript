3498ad9acb1cd4019caae180ba74369976a79335
Holovchyk <golovchuk.misha@gmail.com>
2020-04-07 15:10:47 +0300

    initial commit

--- hwJS_2/task-№2_1-6.js
+++ hwJS_2/task-№2_1-6.js
@@ -1,48 +1,48 @@
-// const logItems = function(array) {
+const logItems = function(array) {
 
-//   for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i++) {
 
-//     console.log(`${i + 1} - ${array[i]}`);
-//   }
-// };
-
-
-// logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
-
-// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+    console.log(`${i + 1} - ${array[i]}`);
+  }
+};
 
 
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 
 
-// const calculateEngravingPrice = function(message, pricePerWord) {
-//   const array = message.split(" ");
-//   const totalPrice = array.length * Number(pricePerWord);
-//   console.log(message);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-//   return totalPrice;
-// };
-// console.log(
-//   calculateEngravingPrice(
-//     "Proin sociis natoque et magnis parturient montes mus",
-//     10
-//   )
-// ); // 80
 
-// console.log(
-//   calculateEngravingPrice(
-//     "Proin sociis natoque et magnis parturient montes mus",
-//     20
-//   )
-// ); // 160
+const calculateEngravingPrice = function(message, pricePerWord) {
+  const array = message.split(" ");
+  const totalPrice = array.length * Number(pricePerWord);
+  console.log(message);
 
-// console.log(
-//   calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
-// ); // 240
+  return totalPrice;
+};
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    10
+  )
+); // 80
 
-// console.log(
-//   calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
-// ); // 120
+console.log(
+  calculateEngravingPrice(
+    "Proin sociis natoque et magnis parturient montes mus",
+    20
+  )
+); // 160
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 40)
+); // 240
+
+console.log(
+  calculateEngravingPrice("Donec orci lectus aliquam est magnis", 20)
+); // 120
 
 
 
@@ -52,105 +52,103 @@
 
 
 
-// const findLongestWord = function(string) {
-//   let stringSplit = string.split(" ");
-//   let total = 0;
-//   let longestWord;
+const findLongestWord = function(string) {
+  let stringSplit = string.split(" ");
+  let total = 0;
+  let longestWord;
 
-//   for (let i = 0; i < stringSplit.length; i += 1) {
-//     total = stringSplit[1].length;
+  for (let i = 0; i < stringSplit.length; i += 1) {
+    total = stringSplit[1].length;
 
-//     if (stringSplit[i].length > total) {
-//       longestWord = stringSplit[i];
+    if (stringSplit[i].length > total) {
+      longestWord = stringSplit[i];
 
-//       return longestWord;
-//     }
-//   }
-// };
+      return longestWord;
+    }
+  }
+};
 
-// console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog")); // 'jumped'
 
-// console.log(findLongestWord("Google do a roll")); // 'Google'
+console.log(findLongestWord("Google do a roll")); // 'Google'
 
-// console.log(findLongestWord("May the force be with you")); // 'force'
-
-
-
-// //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-// "use strict";
-
-// const formatString = function(string) {
-//   if (string.length <= 40) {
-//     return string;
-//   } else {
-//     let longsSrtings = string.slice(0, 41);
-//     console.log(longsSrtings + "...");
-//   }
-// };
-
-// console.log(formatString("Curabitur ligula sapien, tincidunt non."));
-// // вернется оригинальная строка
-
-// console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
-// // вернется форматированная строка
-
-// console.log(formatString("Curabitur ligula sapien."));
-// // вернется оригинальная строка
-
-// console.log(
-//   formatString(
-//     "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
-//   )
-// );
-// // вернется форматированная строка
+console.log(findLongestWord("May the force be with you")); // 'force'
 
 
 
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+"use strict";
+
+const formatString = function(string) {
+  if (string.length <= 40) {
+    return string;
+  } else {
+    let longsSrtings = string.slice(0, 41);
+    console.log(longsSrtings + "...");
+  }
+};
+
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// вернется оригинальная строка
+
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// вернется форматированная строка
+
+console.log(formatString("Curabitur ligula sapien."));
+// вернется оригинальная строка
+
+console.log(
+  formatString(
+    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+  )
+);
+// вернется форматированная строка
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 
 
-// const checkForSpam = function(message) {
-//   message = message.toLowerCase();
-//   if (message) {
-//     message = message.includes("sale") || message.includes("spam");
-//     return message;
-//   }
-// };
+const checkForSpam = function(message) {
+  message = message.toLowerCase();
+  if (message) {
+    message = message.includes("sale") || message.includes("spam");
+    return message;
+  }
+};
 
-// console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("Latest technology news")); // false
 
-// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
 
-// console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Get best sale offers now!")); // true
 
-// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-
-
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
 
-// let input;
-// const numbers = [];
-// let total = 0;
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-// while (true) {
-//   input = prompt("введите число!!!");
-//   if (input === null) {
-//     for (let i = 0; i < numbers.length; i++) {
-//       total += numbers[i];
-//     }
-//     break;
-//   }
-//   input = Number(input)
-//   numbers.push(input);
-// }
-// console.log(`Общая сумма чисел равна ${total}`);
+let input;
+const numbers = [];
+let total = 0;
+
+while (true) {
+  input = prompt("введите число!!!");
+  if (input === null) {
+    for (let i = 0; i < numbers.length; i++) {
+      total += numbers[i];
+    }
+    break;
+  }
+  input = Number(input)
+  numbers.push(input);
+}
+console.log(`Общая сумма чисел равна ${total}`);
 
 
 
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- hwJS_4/task-№4_1-2.js
+++ hwJS_4/task-№4_1-2.js
@@ -1,61 +1,61 @@
-// const account = {
-//   owner: "Mango",
-//   balance: 24000,
-//   discount: 0.1,
-//   orders: ["order-1", "order-2", "order-3"],
-//   changeDiscount(value) {
-//     this.discount = value;
-//   },
-//   showOrders() {
-//     return this.orders;
-//   },
-//   addOrder(cost, order) {
-//     this.balance -= cost;
-//     this.orders.push(order);
-//   },
-// };
+const account = {
+  owner: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["order-1", "order-2", "order-3"],
+  changeDiscount(value) {
+    this.discount = value;
+  },
+  showOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost;
+    this.orders.push(order);
+  },
+};
 
-// account.changeDiscount(0.15);
-// console.log(account.discount);
+account.changeDiscount(0.15);
+console.log(account.discount);
 
-// console.table(account.showOrders());
+console.table(account.showOrders());
 
-// account.addOrder(5000, "order-4");
-// console.log(account.balance);
-// console.table(account.showOrders());
-
-
-//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+account.addOrder(5000, "order-4");
+console.log(account.balance);
+console.table(account.showOrders());
 
 
-// const inventory = {
-//   items: ["Knife", "Gas mask"],
-//   add(itemName) {
-//     console.log(`Adding ${itemName} to inventory`);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-//     this.items.push(itemName);
-//   },
-//   remove(itemName) {
-//     console.log(`Removing ${itemName} from inventory`);
 
-//     this.items = this.items.filter((item) => item !== itemName);
-//   },
-// };
+const inventory = {
+  items: ["Knife", "Gas mask"],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
 
-// const invokeInventoryAction = function (itemName, action) {
-//   console.log(`Invoking action on ${itemName}`);
-//   action.bind(inventory)(itemName); // BIND(inventory)
-// };
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
 
-// invokeInventoryAction("Medkit", inventory.add);
-// // Invoking action on Medkit
-// // Adding Medkit to inventory
+    this.items = this.items.filter((item) => item !== itemName);
+  },
+};
 
-// console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+const invokeInventoryAction = function (itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action.bind(inventory)(itemName); // BIND(inventory)
+};
 
-// invokeInventoryAction("Gas mask", inventory.remove);
-// // Invoking action on Gas mask
-// // Removing Gas mask from inventory
+invokeInventoryAction("Medkit", inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
 
-// console.log(inventory.items); // ['Knife', 'Medkit']
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction("Gas mask", inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
 
